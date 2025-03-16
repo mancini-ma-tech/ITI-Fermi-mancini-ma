cc4aa18b1a13484dc134095c652b2473b85f5f4e
Prowler125 <massimiliamomancini@gmail.com>
2025-03-16 12:28:15 +0100

    Migliora la formattazione del codice e semplifica le funzioni nel file codiceCesare.py

--- 3-BIF#2024-2025/CORSI-VARIE/CorsoCyberSecurity/codiceCesare.py
+++ 3-BIF#2024-2025/CORSI-VARIE/CorsoCyberSecurity/codiceCesare.py
@@ -1,6 +1,6 @@
 import os
 
-
+# Funzione per cambiare il caso delle lettere nel file
 def change_case(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
@@ -17,7 +17,7 @@ def change_case(file_path):
     with open(file_path, 'w') as file:
         file.write(new_content)
 
-
+# Funzione per riformattare il testo rimuovendo spazi extra
 def reformat_text(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
@@ -27,7 +27,7 @@ def reformat_text(file_path):
     with open(file_path, 'w') as file:
         file.write(new_content)
 
-
+# Funzione per crittografare il testo usando il Codice di Cesare
 def caesar_cipher(file_path, shift):
     with open(file_path, 'r') as file:
         content = file.read()
@@ -37,18 +37,16 @@ def caesar_cipher(file_path, shift):
         if char.isalpha():
             shift_amount = shift % 26
             if char.islower():
-                new_content += chr((ord(char) - ord('a') +
-                                   shift_amount) % 26 + ord('a'))
+                new_content += chr((ord(char) - ord('a') + shift_amount) % 26 + ord('a'))
             elif char.isupper():
-                new_content += chr((ord(char) - ord('A') +
-                                   shift_amount) % 26 + ord('A'))
+                new_content += chr((ord(char) - ord('A') + shift_amount) % 26 + ord('A'))
         else:
             new_content += char
 
     with open(file_path, 'w') as file:
         file.write(new_content)
 
-
+# Funzione principale che gestisce il menu e le scelte dell'utente
 def main():
     file_path = input("Inserisci il percorso del file (.cpp, .txt, .py): ")
 
@@ -70,8 +68,7 @@ def main():
         reformat_text(file_path)
         print("Il testo è stato riformattato con successo.")
     elif choice == '3':
-        shift = int(
-            input("Inserisci il numero di posizioni per il Codice di Cesare (N): "))
+        shift = int(input("Inserisci il numero di posizioni per il Codice di Cesare (N): "))
         if 0 < shift < 26:
             caesar_cipher(file_path, shift)
             print("Il testo è stato crittografato con successo.")
@@ -79,7 +76,6 @@ def main():
             print("Il numero di posizioni deve essere compreso tra 1 e 25.")
     else:
         print("Scelta non valida.")
-
 
 if __name__ == "__main__":
     main()
