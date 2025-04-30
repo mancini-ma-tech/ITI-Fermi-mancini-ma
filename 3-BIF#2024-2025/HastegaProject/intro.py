99a85afde72b703a8b9f6c4d2132ae1fb15ba34f
prowler125 <massimiliamomancini@gmail.com>
2025-04-30 11:26:13 +0200

    Aggiungi script intro.py: stampa messaggi di saluto, gestisce variabili e modifica una lista di frutti.

---
+++ 3-BIF#2024-2025/HastegaProject/intro.py
@@ -0,0 +1,13 @@
+print("Hello, world!!!")
+età = 17
+nome = "Massimiliano"
+punti = 20.3
+print("Ciao, " + nome + "! Hai " + str(età) + "anni.")
+print("Hai totalizzato " + str(punti) + " punti.")
+lista_frutti = ("Mele", "Pere", "Banane")
+print("Lista originale dei frutti: ", lista_frutti)
+lista_frutti.append("Kiwi")
+print("Lista dopo averla modificata: ", lista_frutti)
+lista_frutti.insert(1, "Mango")
+print("Lista dopo averla modificata: ", lista_frutti)
+lista_frutti.remove("Banane")
