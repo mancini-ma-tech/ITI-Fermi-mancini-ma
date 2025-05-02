6385fb54644c443ae69035d7f08028ef2045011a
prowler125 <massimiliamomancini@gmail.com>
2025-05-02 12:26:56 +0200

    Correggi la posizione dell'alert per mostrare la somma solo dopo il calcolo

--- 3-BIF#2024-2025/INFORMATICA/HTML/JavaScript/Ex00-B.js
+++ 3-BIF#2024-2025/INFORMATICA/HTML/JavaScript/Ex00-B.js
@@ -4,7 +4,7 @@ let b = parseInt(prompt("Inserisci il 2° numero:"));
 if (!isNaN(a) && !isNaN(b)) {
   let somma = a + b;
   console.log("La somma dei due numeri è:", somma);
+  alert(somma);
 } else {
   console.log("Per favore, inserisci solo numeri validi.");
 }
-alert(somma);
