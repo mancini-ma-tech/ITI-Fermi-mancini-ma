196c2cc00fdace3f6fb2fd28b375ed8eb4a5c111
prowler125 <massimiliamomancini@gmail.com>
2025-05-08 08:59:35 +0200

    Aggiungi file HTML e JavaScript per l'esercizio di concatenazione di stringhe

---
+++ 3-BIF#2024-2025/INFORMATICA/HTML/JavaScript/Es04.js
@@ -0,0 +1,5 @@
+var a = "Ciao "; // doppi apici
+var b = 'Lucca'; // singoli apici
+
+// Unisce le due stringhe e le scrive nell'elemento con ID "parola"
+document.getElementById("parola").innerHTML = a + b;
