41397fa3500a5cd13b78a7bd4846972a64a0ba1d
prowler125 <massimiliamomancini@gmail.com>
2025-09-24 13:39:06 +0200

    Aggiungi la classe Moto per la gestione dei modelli di moto

---
+++ 3-BIF#2024-2025/INFORMATICA/JAVA/Moto.java
@@ -0,0 +1,39 @@
+/*
+ Classe Mot
+ JAVA
+*/
+
+
+
+public class Moto {
+  String modello;
+  Moto(String m) {
+      this.modello = m;  
+  }
+  
+  public static void main(String[] args) {
+    Moto m1 = new Moto("BMV");
+    Moto m2 = new Moto("Mercedesi");
+    System.out.println(m2.modello);
+    System.out.println(m1.modello);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
